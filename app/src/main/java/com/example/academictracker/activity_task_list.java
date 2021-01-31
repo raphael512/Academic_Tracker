0445797d5c779bc444e51e97c0354d8afc2a5911
raphael512 <raphael512.rg@gmail.com>
2021-02-01 05:50:18 +0800

    INAYOS KO YUNG activity_task_list.xml saka activity_task_list.java

--- app/src/main/java/com/example/academictracker/activity_task_list.java
+++ app/src/main/java/com/example/academictracker/activity_task_list.java
@@ -1,14 +1,20 @@
 package com.example.academictracker;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.util.List;
 
 public class activity_task_list extends AppCompatActivity {
-
+    int x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +22,29 @@ public class activity_task_list extends AppCompatActivity {
 
         DataBaseHelper db = new DataBaseHelper(activity_task_list.this);
         List<String> list = db.getList();
+        CheckBox[] checkBox = new CheckBox[list.size()];
+        LinearLayout lnrLayout = (LinearLayout) findViewById(R.id.lnrLayout);
 
+        for(x = 0; list.isEmpty() == false; x++){
+            if(list.size() == x){
+                break;
+            }
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams((int) LinearLayout.LayoutParams.WRAP_CONTENT, (int) LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.topMargin = 50;
+            params.leftMargin = 50;
+            checkBox[x] = new CheckBox(this);
+            checkBox[x].setText(list.get(x));
+            checkBox[x].setTextSize((float) 20);
+            checkBox[x].setLayoutParams(params);
+            checkBox[x].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    lnrLayout.removeView(buttonView);
+                }
+            });
+
+            lnrLayout.addView(checkBox[x]);
+        }
         //Toast.makeText(activity_task_list.this, list.toString(), Toast.LENGTH_SHORT);
     }
 }
