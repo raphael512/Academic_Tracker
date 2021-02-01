5690b4fe2a1a85e7a038ad61efbde7d2c4cb9cdf
ORJAN <orjanchua19@gmail.com>
2021-02-01 17:11:57 +0800

    latom

---
+++ app/src/main/java/com/example/academictracker/CheckBox.java
@@ -0,0 +1,21 @@
+package com.example.academictracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CheckBox extends AppCompatActivity {
+
+    CheckBox checkBox_venti;
+    
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recycler_view);
+
+
+//        checkBox_venti  = findViewById(R.id.checkBox_checkBox);
+        
+        
+
+    }
+
+}
---
+++ app/src/main/java/com/example/academictracker/ToDoAdapter.java
@@ -0,0 +1,53 @@
+package com.example.academictracker;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+//import com.example.academictracker.CheckBox;
+import com.example.academictracker.*;
+
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder>{
+    @NonNull
+    @Override
+    public ToDoAdapter.ViewHolder onCreateViewHolder( ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.checkbox, viewGroup, false);
+        return new ViewHolder(itemView);
+    }
+
+    /**
+     *
+     * Kukuha ng data sa List
+     */
+    @Override
+    public void onBindViewHolder(@NonNull ToDoAdapter.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    /**
+     *
+     * Lagay mo nga yung checkbox galing checkbox xml di ko magawa ehe..
+     *
+     */
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        CheckBox checkBox_venti;
+
+        public ViewHolder(@NonNull View view) {
+            super(view);
+            checkBox_venti = view.findViewById();
+
+        }
+    }
+}
