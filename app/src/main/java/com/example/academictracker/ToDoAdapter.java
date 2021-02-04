fd579ae3d92ad63f83ce23a2410d383aef8f8349
ORJAN <orjanchua19@gmail.com>
2021-02-04 10:26:41 +0800

    viewholder edit

--- app/src/main/java/com/example/academictracker/ToDoAdapter.java
+++ app/src/main/java/com/example/academictracker/ToDoAdapter.java
@@ -6,12 +6,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 //import com.example.academictracker.CheckBox;
 import com.example.academictracker.*;
 
+import java.util.List;
+
 
 public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder>{
+
+    private List<TaskModel> task_list;
+
+    DataBaseHelper db;
+    MainActivity activity;
+
+    public ToDoAdapter(DataBaseHelper db, MainActivity activity) {
+        this.db = db;
+        this.activity = activity;
+    }
+
+
     @NonNull
     @Override
     public ToDoAdapter.ViewHolder onCreateViewHolder( ViewGroup viewGroup, int i) {
@@ -27,7 +42,41 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder>{
     @Override
     public void onBindViewHolder(@NonNull ToDoAdapter.ViewHolder viewHolder, int i) {
 
+        /**
+         * position sa data set?????
+         *
+         */
+        final TaskModel item = task_list.get(position);
+
+        viewHolder.checkBox_venti.setText(item.getTaskName());
+
+        /**
+         * int getStatus
+         */
+//        viewHolder.checkBox_venti.setChecked(toBoolean(item.getStatus()));
+
+
+        viewHolder.checkBox_venti.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+
+                }
+
+                else {
+
+                }
+            }
+        });
+
     }
+
+
+
+    private boolean toBoolean(int n) {
+        return n != 0;
+    }
+
 
     @Override
     public int getItemCount() {
